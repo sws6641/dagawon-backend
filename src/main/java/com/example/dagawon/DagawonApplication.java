5f4c414709a6f93f60a4bc2e87fd4d34b6248415
sws6641 <sws6641@naver.com>
2025-09-21 21:07:54 +0900

    프로젝트 세팅

---
+++ src/main/java/com/example/dagawon/DagawonApplication.java
@@ -0,0 +1,13 @@
+package com.example.dagawon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DagawonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DagawonApplication.class, args);
+	}
+
+}
