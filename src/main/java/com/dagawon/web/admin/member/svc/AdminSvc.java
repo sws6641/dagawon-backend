0853f3ac78b2a0f0b1d8eff3b76b2cc6a78517e0
songwonseop <sws6641@bankle.co.kr>
2025-11-21 12:38:55 +0900

    feat(user): 회원정보 저장 API 작업중

--- src/main/java/com/dagawon/web/admin/member/svc/AdminSvc.java
+++ src/main/java/com/dagawon/web/admin/member/svc/AdminSvc.java
@@ -1,11 +1,14 @@
 package com.dagawon.web.admin.member.svc;
 
 
+import com.dagawon.web.admin.member.vo.AdminVo;
+import com.dagawon.web.common.mapper.TbMembMapper;
 import com.dagawon.web.common.repo.TbMembRepository;
-import com.dagawon.web.config.exception.DefaultException;
+import com.dagawon.web.common.util.CustomeModelMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * 어드민
@@ -14,6 +17,10 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class AdminSvc {
+
+    private final TbMembMapper tbMembMapper;
+    private final CustomeModelMapper customeModelMapper;
+
     private final TbMembRepository tbMembRepository;
 
 
@@ -44,7 +51,44 @@ public class AdminSvc {
         return exists ? "00" : "01";
     }
 
+    @Transactional
+    public String crtMembers(AdminVo.CrtMembReq crtMembReqVo) {
 
+        // TODO: 계정 중복 체크는 위 서비스 사용
+        // 1. 이메일 중복 체크
+//        if (tbMembRepository.existsByEmailId(crtMembReqVo.getEmailId())) {
+//            throw new IllegalArgumentException("이미 사용 중인 이메일 아이디입니다.");
+//        }
+
+        // TODO: 사번 생성은 프로시저 사용?
+        // 2. 사번 생성
+//        String empNo = generateEmpNo();
+
+        // 3. DTO 생성
+//        TbMembDto dto = TbMembDto.builder()
+//                .membNm(crtMembReqVo.getUserName())
+//                .membEmail(crtMembReqVo.getEmailId())
+//                .deptCd(crtMembReqVo.getDeptCd())
+//                .rankCd(crtMembReqVo.getRankCd())
+//                .empNo(empNo)
+//                .build();
+
+        // 4. 저장
+//        tbMembRepository.save(tbMembMapper.toEntity(dto));
+
+//        return empNo;   // 생성된 사번 반환
+        return "";   // 생성된 사번 반환
+    }
+
+//    private String generateEmpNo() {
+//        String date = LocalDate.now().format(DateTimeFormatter.ofPattern("yyyyMMdd"));
+//
+//        // 오늘 날짜에 해당하는 seq 체크
+//        int countToday = tbMembRepository.countByEmpNoStartingWith("EMP" + date);
+//
+//        String seq = String.format("%04d", countToday + 1);
+//        return "EMP" + date + seq;
+//    }
 
 
 }
--- src/main/java/com/dagawon/web/admin/member/vo/AdminVo.java
+++ src/main/java/com/dagawon/web/admin/member/vo/AdminVo.java
@@ -1,10 +1,33 @@
 package com.dagawon.web.admin.member.vo;
 
 
-
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
 
 public class AdminVo {
+    @Getter
+    @Setter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CrtMembReq {
+        @NotEmpty(message = "ID는 필수 항목입니다.")
+        @Schema(name = "id", example = "sws6641")
+        private String userName;
 
+        @NotEmpty(message = "PASSWORD는 필수 항목입니다.")
+        @Schema(name = "pwd", example = "1234")
+        private String emailId;
+
+        @NotEmpty(message = "구분 코드는 필수 항목입니다.")
+        @Schema(name = "membGbCd", example = "00")
+        private String deptCd;
+
+        @NotEmpty(message = "구분 코드는 필수 항목입니다.")
+        @Schema(name = "membGbCd", example = "00")
+        private String rankCd;
+    }
 
 
 
